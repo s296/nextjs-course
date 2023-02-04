e00b0841c7a0a3511c301cfbb8964b21f03790ab
shubhamg.contractor <shubhamg.contractor@atlashxm.com>
2023-02-04 11:36:02 +0530

    file base routing

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+function AboutPage(){
+    return <div>
+    <h1> aboutPage</h1>
+  </div>
+}
+
+export default AboutPage;
---
+++ pages/portfolio/index.js
@@ -0,0 +1,7 @@
+function PortfolioPage() {
+    return <div>
+      <h1> PortfolioPAge</h1>
+    </div>
+  }
+  
+  export default PortfolioPage;
